30c7b0cc57dfe92778440307681d91f0e940b649
Vaishnavi Bhardwaj <vaishnavi.bhardwaj@innobitystems.com>
2023-05-19 11:06:46 +0530

    Achnage USer Module

--- src/routes.ts
+++ src/routes.ts
@@ -1,12 +1,8 @@
 import { Router } from "express"
-import userRoutes from "./modules/users/routes/userRoutes"
 
 const mainRouter = Router()
 
 // add module's router here in main router
-
-
- mainRouter.use(userRoutes)
 
 
 export default mainRouter
